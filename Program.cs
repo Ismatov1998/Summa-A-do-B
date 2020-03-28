0ed7762df78947d876d9a861b0830c7306af64cb
Азиз Исматов <yourGitHub@example.co>
2020-03-28 19:26:48 +0500

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,S=0;
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            for(int i=a;i<=b;i++)
+            {
+                S=S+i;
+            }
+            Console.WriteLine(S);
+            
+        }
+    }
+}
